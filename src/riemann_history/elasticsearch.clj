7ff43ec6a46000dab120a3cbfabbbcde423a303d
bhois <bernd.hois@bearingpoint.com>
2018-06-25 22:09:07 +0200

    Adding some tests

---
+++ src/riemann_history/elasticsearch.clj
@@ -0,0 +1,1 @@
+(ns riemann-history.elasticsearch)
--- test/riemann_history/core_test.clj
+++ test/riemann_history/core_test.clj
@@ -1,2 +1,14 @@
-(ns riemann-history.core-test)
+(ns riemann-history.core-test
+  (:require [riemann-history.core :as history]
+            [clojure.test :refer :all]))
+
+(deftest transform
+  (is (= (history/transform [{:key "1:0" :doc_count 55} 
+                             {:key "1:2" :doc_count 100} 
+                             {:key "5:6" :doc_count 75}])
+         {:1:0 55 :1:2 100 :5:6 75})))
+
+(deftest epoch
+  (is (= (history/generate-key-from-epoch 1529932642.494)
+         :1:13)))
 
