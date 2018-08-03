ec7f262472b9ba1184e127a367ce3cb2b55630a8
boernd <boernd@users.noreply.github.com>
2018-08-03 12:05:08 +0200

    Fix timezone issue (#6)

--- test/riemann_history/core_test.clj
+++ test/riemann_history/core_test.clj
@@ -8,8 +8,10 @@
                              {:key "5:6" :doc_count 75}])
          {:1:0 55 :1:2 100 :5:6 75})))
 
-(deftest epoch
-  (is (= (history/generate-key-from-epoch 1529932642.494)
+(deftest epoch-tz-default
+  (is (= (history/generate-key 1529932642.494)
          :1:13)))
 
-; spec: guide on clojure.org
+(deftest epoch-tz-europe-vienna
+  (is (= (history/generate-key 1529932642.494 "Europe/Vienna")
+         :1:15)))
